c48e0ace010d3c3a308bb7407d431d537ff95837
Daniel Khakbaz <danialkhakbaz.dk@gmail.com>
2024-07-10 14:26:31 +0330

    (feat): Add error and not-found style

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Link from "next/link";
+
 type ErrorProps = {
   error: Error;
   reset: () => void;
@@ -7,11 +9,29 @@ type ErrorProps = {
 
 const Error = ({ error, reset }: ErrorProps) => {
   return (
-    <div>
-      <p>{error.message}</p>
-      <button onClick={reset}>
-        <span>Try Again</span>
-      </button>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-12">
+        <h2 className="text-2xl">There is a problem!</h2>
+        <pre className="w-5/6 whitespace-pre-wrap bg-black text-center text-white">
+          {error.message}
+        </pre>
+        <h4 className="text-lg">
+          Please try again. If the problem persists, fell free to contact me!
+        </h4>
+        <div className="flex items-center justify-center gap-4">
+          <button
+            className="flex items-center gap-4 rounded-md bg-purple-400 p-3 transition-colors duration-150 hover:bg-purple-500"
+            onClick={reset}
+          >
+            <span>Try Again</span>
+          </button>
+          <Link href="/">
+            <button className="rounded-md bg-yellow-400 p-3 transition-colors duration-150 hover:bg-yellow-500">
+              Back to the Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 const NotFound = async () => {
-  return <></>;
+  return (
+    <div className="flex h-screen w-screen flex-col justify-center gap-8 py-8 text-center">
+      <b className="mb-4 text-2xl font-bold">NOT FOUND!</b>
+      <h3 className="text-[12rem] font-extrabold">404</h3>
+      <div className="mt-8 flex flex-col text-center">
+        <Link href="/">
+          <button className="rounded-md bg-yellow-400 p-3 transition-colors duration-150 hover:bg-yellow-500">
+            Back to the Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
